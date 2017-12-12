09b788ea0b4463ba236ae879f3622ae80ad7d65c
tojo009 <tojo.joseph009@gmail.com>
2017-12-12 17:14:13 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,19 @@
 from subprocess import check_output
-
+from lxml import etree
 
 currentCommitSHA  = check_output("git rev-parse HEAD",shell=True).decode(encoding='UTF-8').rstrip()
-#masterSHA        = check_output("git rev-parse orgin/master",shell=True)
 print(currentCommitSHA)
-#print(masterSHA)
+dtd = etree.DTD("sailpoint.dtd")
 
 command           = "git diff --name-only "+ currentCommitSHA +" origin/master~0"
 filesCheckResult  = check_output(command,shell=True)
-modifiedFiles     = filesCheckResult.decode(encoding='UTF-8').split("\n").remove('')
+modifiedFiles     = filesCheckResult.decode(encoding='UTF-8').split("\n")
+for file in modifiedFiles:
+   if file.endswith(".xml"):
+      print("Validating -" + file)
+      with open(file, 'r') as myfile:
+      data=myfile.read().replace('\n', '')
+      root = etree.XML(data)
+      print(dtd.validate(root))
+  
 print(modifiedFiles)
