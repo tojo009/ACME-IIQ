ec19bf43788cbf6c558eb53fbecfd60c173a3ce5
tojo009 <tojo.joseph009@gmail.com>
2017-12-12 15:59:01 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -5,5 +5,5 @@ currentCommitSHA = check_output("git rev-parse HEAD",shell=True)
 #masterSHA        = check_output("git rev-parse orgin/master",shell=True)
 print(currentCommitSHA)
 #print(masterSHA)
-command = "git diff --name-only fd07f332b27dccff04ecbb69c7c40a0b6fb5bd0a master~0"
+command = "git diff --name-only "+ currentCommitSHA.decode(encoding='UTF-8') +" origin/master~0"
 print(check_output(command,shell=True))
