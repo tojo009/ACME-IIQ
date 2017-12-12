ce84c194f823d979eaae691e09f8864433e0972a
tojo009 <tojo.joseph009@gmail.com>
2017-12-12 15:22:31 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,8 @@ from subprocess import check_output
 
 
 currentCommitSHA = check_output("git rev-parse HEAD",shell=True)
+masterSHA        = check_output("git rev-parse orgin/master",shell=True)
 print(currentCommitSHA)
-command = "git diff --name-only" + currentCommitSHA + "master"
+print(masterSHA)
+command = "git diff --name-only" + currentCommitSHA + "orgin/master~0"
 print(check_output(command,shell=True))
